a75c848747bb210eeec43b68641d1cb1fccb7832
Rituraj27 <ri2rajhao1@gmail.com>
2025-06-13 22:01:24 +0530

    vercel

--- FrontEnd/spring-react-frontend/server.js
+++ FrontEnd/spring-react-frontend/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? 'https://your-vercel-domain.vercel.app'
+        ? 'https://employee-management-system-seven-rho.vercel.app'
         : 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
